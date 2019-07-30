c31e121daa446da071e9454acd49b7a0190a1a66
Haverzard <yonatanviody@gmail.com>
2019-07-30 17:15:30 +0700

    IF2018DB v.1.2.0 - Responsive Update

---
+++ UPDATE.md
@@ -0,0 +1,19 @@
+# v.1.1.1 - Added Home Page and Footer
+Added:
+	- Home Page
+	- Footer
+
+Fixed:
+	- Position Values
+	- Some HTML Elements
+
+# v.1.1.0 - Mobile Interface Added
+Added:
+	- Mobile Interface for K1,K2,K3,All Pages
+	- Background Images
+
+# v.1.0.0 - Beta Launch
+Added:
+	- K1 Data
+	- K2 Data
+	- K3 Data
---
+++ static/js/map.js
@@ -0,0 +1,36 @@
+h = window.innerHeight;
+w = window.innerWidth;
+document.getElementById("menu").style.height = h/10 + "px";
+
+for (i = 0; i < document.getElementsByClassName("title").length; i++) {
+	document.getElementsByClassName("title")[i].style.fontSize = w*0.03 + "px";
+}
+
+for (i = 0; i < document.getElementsByClassName("text").length; i++) {
+	document.getElementsByClassName("text")[i].style.fontSize = w*0.01 + "px";
+}
+
+for (i = 0; i < document.getElementsByClassName("menu_box").length; i++) {
+	document.getElementsByClassName("menu_box")[i].style.width = w*0.2 + "px";
+	document.getElementsByClassName("menu_box")[i].style.fontSize = w*0.015 + "px";
+}
+
+if (location.pathname == "/") {
+	document.getElementById("container2").style.width = w*0.5 + "px";
+	document.getElementById("container2").style.height = w*0.25 + "px";
+	document.getElementById("container2").style.top = (h-w*0.25)/2 + "px";
+	document.getElementById("container2").style.left = w*0.25 + "px";
+} else if (location.pathname[1] == "k") {
+	for (i = 0; i < document.getElementsByClassName("name_container").length; i++) {
+		document.getElementsByClassName("name_container")[i].style.width = w*0.9*0.3 + "px";
+	}
+	for (i = 0; i < document.getElementsByClassName("footer_text").length; i++) {
+		document.getElementsByClassName("footer_text")[i].style.fontSize = w*0.01 + "px";
+	}
+	document.getElementById("container").style.width = w*0.9 + "px";
+	document.getElementById("container").style.top = w*0.05 + "px";
+	document.getElementById("container").style.left = w*0.05 + "px";
+	document.getElementById("context").style.margin = w*0.9*0.05 + "px";
+	document.getElementById("context2").style.margin = w*0.9*0.05 + "px";
+	document.getElementById("footer").style.height = w*0.05 + "px";
+}
