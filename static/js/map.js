be7411063713f35c84ed41c4ccafda8cce918c77
Haverzard <yonatanviody@gmail.com>
2019-07-30 22:11:41 +0700

    IF2018DB v.1.2.5 - Font Sizing - (4)

--- static/js/map.js
+++ static/js/map.js
@@ -5,7 +5,7 @@ sc = 1;
 
 if (h > w) { sc=2; };
 if (location.pathname == "/") {
-	document.getElementById("container2").style.width = w*0.5 + "px";
+	document.getElementById("container2").style.width = w*0.5*sc + "px";
 	document.getElementById("container2").style.height = w*0.25 + "px";
 	document.getElementById("container2").style.top = (h-w*0.25)/2 + "px";
 	document.getElementById("container2").style.left = w*0.25 + "px";
@@ -18,7 +18,7 @@ if (location.pathname == "/") {
 		document.getElementsByClassName("footer_text")[i].style.fontSize = w*0.01 + "px";
 	}
 	document.getElementById("footer_box").style.top = w*(0.05-0.01*document.getElementsByClassName("footer_text").length)/2 + "px";
-	document.getElementById("container").style.width = w*0.8 + "px";
+	document.getElementById("container").style.width = w*0.9 + "px";
 	document.getElementById("container").style.top = w*0.05 + "px";
 	document.getElementsByClassName("content")[0].style.margin = w*0.8*0.05 + "px";
 	document.getElementsByClassName("content")[1].style.margin = w*0.8*0.05 + "px";
