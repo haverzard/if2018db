45d0f9bcdcc3f84bcc0faaec45ece0048e60b32f
Haverzard <yonatanviody@gmail.com>
2019-07-30 23:02:02 +0700

    IF2018DB v.1.3.0 - Mobile and PC Interface Finished

--- static/js/map.js
+++ static/js/map.js
@@ -24,8 +24,8 @@ if (location.pathname == "/") {
 	document.getElementById("footer").style.height = w*0.05 + "px";
 	if (h > w) {
 		document.getElementsByClassName("content")[0].remove();
-	document.getElementById("container").style.width = w + "px";
-	document.getElementsByClassName("content")[1].style.margin = w*0.05 + "px";
+		document.getElementById("container").style.width = w + "px";
+		document.getElementsByClassName("content")[0].style.margin = w*0.05 + "px";
 	} else {
 		document.getElementsByClassName("content")[1].remove();
 		document.getElementById("container").style.width = w*0.8 + "px";
